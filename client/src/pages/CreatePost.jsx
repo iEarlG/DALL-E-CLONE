911273c068e5715c7155760af02f51fa90efda3e
Earl Villapaz <jasonburning16@gmail.com>
2023-02-02 21:10:41 +0800

    working on CreatePost and utils

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({name: '', prompt: '', photo: ''});
+  const [generatingImg, setGeneratingImg] = useState(false);
+
   return (
     <div>CreatePost</div>
   )
