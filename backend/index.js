0382dea74705e68e651c522e62d59cb1be7e65c4
Earl Villapaz <jasonburning16@gmail.com>
2023-02-05 19:43:20 +0800

    adding models, routes

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongoose/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import appRoutes from './routes/appRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/app', appRoutes);
 
 app.get('/', async (req, res) => {
     res.send("IM HERE!!!!!!!!!!!!!!!!!");
