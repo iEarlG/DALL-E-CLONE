0e8e3116d2934dbfe24d06a9b8e78b312d67dc20
Earl Villapaz <jasonburning16@gmail.com>
2023-02-05 19:59:14 +0800

    adding the appRoutes functionality. Backend to Client side

--- backend/routes/appRoutes.js
+++ backend/routes/appRoutes.js
@@ -5,3 +5,35 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEYS,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send("DALL-E- 2 API is working!");
+});
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const AIresponse = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+
+        const image = AIresponse.data.data[0].b64_json;
+
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message);
+    }
+});
+
+export default router;
