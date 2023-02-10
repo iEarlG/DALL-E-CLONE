3abf0e7ba03d572c84c40a67706e667165378330
Earl Villapaz <jasonburning16@gmail.com>
2023-02-10 20:23:11 +0800

    adding the uploaded url from demo to live url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,7 +18,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try { 
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-2-clone.onrender.com/api/v1/post", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -48,7 +48,7 @@ const CreatePost = () => {
     if(form.prompt) { 
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', { 
+        const response = await fetch('https://dall-e-2-clone.onrender.com/api/v1/dalle', { 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
